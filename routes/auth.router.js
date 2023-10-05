b7b9140daee8f4487de563cd96ff9486e07c06aa
DaveJuanes <dmjuanes_@hotmail.es>
2023-10-04 23:58:44 -0400

    [CLASE 17]

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -3,7 +3,9 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const { config } = require('./../config/config');
 
+const AuthService = require('./../services/auth.service')
 const router = express.Router();
+const service = new AuthService()
 
 router.post(
   '/login',
@@ -11,15 +13,20 @@ router.post(
   async (req, res, next) => {
     try {
       const user = req.user;
-      const payload = {
-        sub: user.id,
-        role: user.role,
-      };
-      const token = jwt.sign(payload, config.jwtSecret);
-      res.json({
-        user,
-        token,
-      });
+      res.json(service.signToken(user))
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  '/recovery',
+  async (req, res, next) => {
+    try {
+      const {email} = req.body;
+      const rta = await service.sendRecovery(email)
+      res.json(rta);
     } catch (error) {
       next(error);
     }
